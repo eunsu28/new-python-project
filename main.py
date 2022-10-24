4175a9335d37ed07cef002a68d12ad65cbfa2bef
eunsu28 <ayay.e2008@gmail.com>
2022-10-24 19:07:56 +0900

    import sleep

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import os
-clear = lambda : os.system('cls')
-clear()
+from time import sleep
+
+def clear():
+    return os.system("clear")
 
 
 print("this thing will tell you what you've read so far")
 
 a = input("choose the function: ")
+if a == "a":
+    print("hello")
+    sleep(1)
+else:
+    print("bye")
+    sleep(1)
+
+clear()
