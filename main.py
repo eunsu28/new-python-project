accb1eb5005f0dc927b62d184c9436ec5f4cb973
eunsu28 <ayay.e2008@gmail.com>
2022-11-06 19:03:26 +0900

    put things in while

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import os
 from time import sleep
 
+thing = 1
+
 def clear():
     return os.system("clear")
 
+while thing:
+    print("this thing will tell you what you've read so far")
+    
+    f_input = input("choose the function: ")
+    if f_input == "a":
+        print("add things")
+        sleep(1)
+        f = open("book.txt", 'a')
+        s_input = input(":")
+        f.write("%s\n" %s_input)
+        f.close()
+    elif f_input == "r":
+        print("bye")
+        sleep(1)
 
-print("this thing will tell you what you've read so far")
-
-f_input = input("choose the function: ")
-if f_input == "a":
-    print("add things")
-    sleep(1)
-    f = open("book.txt", 'a')
-    s_input = input(":")
-    f.write("%s\n" %s_input)
-    f.close()
-elif f_input == "r":
-    print("bye")
-    sleep(1)
-
-clear()
-#
+    clear()
