3a1804a68b8b8acdd953297beefed3b760cb35c3
eunsu28 <ayay.e2008@gmail.com>
2022-11-14 22:00:09 +0900

    making new funciton

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ while thing == 1:
     print("this thing will tell you what you've read so far")
 
     f_input = input("choose the function: ")
+    ##########
     if f_input == "a":
         print("add things")
         sleep(1)
-        #####################################
+        ##########
         f = open("book.txt", 'a')
         s_input = input(":")
         f.write("%s\n" %s_input)
         f.close()
-        #####################################
+    ##########
     elif f_input == "r":
+        print("I'll load all the books that you've read")
+        sleep(1)
+    ##########
+    elif f_input == 'f':
         thing = 2
 
     clear()
