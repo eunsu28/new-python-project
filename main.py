f154332de5658cbda6773b680e094d998eed68db
eunsu28 <ayay.e2008@gmail.com>
2022-11-17 12:09:56 +0900

    test finished

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ while thing == 1:
         ##########
         f = open("book.txt", 'a')
         s_input = input(":")
+        if s_input == "c":
+            thing = 2
         f.write("%s\n" %s_input)
         f.close()
     ##########
